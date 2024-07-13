882e7e8e8e6b7408ea3aa256e2775dc3061ddb93
Gal <gal.granot95@gmail.com>
2024-07-13 09:35:09 +0000

    started histograms

--- HW1/ex1.cu
+++ HW1/ex1.cu
@@ -1,4 +1,56 @@
+//ex1.cu
+
+/*=============================================================================
+* includes
+=============================================================================*/
 #include "ex1.h"
+
+/*=============================================================================
+* constants
+=============================================================================*/
+#define COLOR_RANGE 256
+#define TILES TILE_COUNT * TILE_COUNT
+#define PIXELS_PER_TILE TILE_WIDTH * TILE_WIDTH
+#define TILES_PER_LINE TILE_COUNT
+
+/*=============================================================================
+* helper functions
+=============================================================================*/
+__device__ inline int calc_offset(int i) { return }
+__device__
+void compute_histograms(uchar *img, int histograms[TILES][COLOR_RANGE]) {
+    int tid = threadIdx.x;
+    for (int i = 0; i < TILES; i++) {
+        if(tid < COLOR_RANGE) {
+            histograms[i][tid] = 0; //FIXME: assuming #threads > COLOR_RANGE
+        }
+    }
+    __syncthreads();
+    /*
+    each thread needs to: 
+        1. read a pixel value
+        2. learn its block index
+        3. atomic write to appropriate histogram
+    */
+   
+   //FIXME: isn't correct right now
+    for (int i = 0; i < TILES; i++) { 
+        int line_offset = ((int)std::round(i / TILES_PER_LINE)) * IMG_WIDTH;
+        int tile_offset = (i % TILES_PER_LINE) * TILE_WIDTH;
+        //FIXME: need to add thread offset inside tile and disperse threads
+        // throughout tile while dropping lines when appropriate
+        int offset = line_offset + tile_offset /*+thread offset*/; 
+        int value = img[offset];
+
+        int tile; //FIXME: calc correct tile
+        atomicAdd_block(&histograms[tile][value], 1);
+    }
+    __syncthreads();
+}
+
+/*=============================================================================
+* required functions
+=============================================================================*/
 
 __global__ void prefix_sum(int arr[], int arr_size) {
     int tid = threadIdx.x;
@@ -24,12 +76,19 @@ __global__ void prefix_sum(int arr[], int arr_size) {
  * @param out_img single output buffer, in global memory.
  */
 __device__ 
-void interpolate_device(uchar* maps ,uchar *in_img, uchar* out_img);
+void interpolate_device(uchar *maps ,uchar *in_img, uchar *out_img);
 
 __global__ void process_image_kernel(uchar *all_in, uchar *all_out, uchar *maps) {
-    // TODO
+    /* steps:
+        1. divide image into tiles
+        2. compute histogram for each tile
+        3. compute cdf for each histogram
+        4. compute map from each pixel value to another
+        5. interpolate_device() and return
+    */
+    __shared__ int histograms[TILE_COUNT][COLOR_RANGE];
     interpolate_device(maps, all_in, all_out);
-    return; 
+    return;
 }
 
 /* Task serial context struct with necessary CPU / GPU pointers to process a single image */
